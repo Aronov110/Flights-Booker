f82a8b083cec488b54ef16528e3e07f5354def8a
Aronov110 <aronov110@gmail.com>
2024-11-28 23:17:04 +0200

    bugfixes v8

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,9 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import path from 'node:path';
 
-export default defineConfig({
-	plugins: [react()],
-	define: {
-		"process.env": process.env,
-	},
-});
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, path.resolve(process.cwd(), 'env'), 'VITE_')
+  
+  return {
+    plugins: [react()],
+
+    envDir: './env',
+    define: {
+      'process.env': env
+    }
+  }
+})
