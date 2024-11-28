81933593e19e6df8c15e9aca647b92780e7f3e26
Aronov110 <aronov110@gmail.com>
2024-11-28 11:00:35 +0200

    trying to fix cors issue

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -6,9 +6,15 @@ import { flightRoutes } from "./routes/flightRoutes.js";
 
 const app = express();
 
+const allowedOrigins = ["https://flights-booker.onrender.com"];
+
+const options: cors.CorsOptions = {
+	origin: allowedOrigins,
+};
+
 app.disable("x-powered-by");
 app.use(helmet());
-app.use(cors());
+app.use(cors(options));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(flightRoutes);
